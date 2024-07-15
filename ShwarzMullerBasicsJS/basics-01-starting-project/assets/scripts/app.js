071ab642426c7c2b17c54d7424977b07f9b8b699
EYMauroBonilla <Mauro.bonilla@mx.ey.com>
2024-07-15 11:33:17 -0600

    app.js with button func.

--- ShwarzMullerBasicsJS/basics-01-starting-project/assets/scripts/app.js
+++ ShwarzMullerBasicsJS/basics-01-starting-project/assets/scripts/app.js
@@ -1,7 +1,83 @@
-let currentResult = 0;
+let defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
 
-currentResult = currentResult + 10;
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
 
-let calculationDescription = 'insert description';
+function createAndWriteOuptut(
+  operator,
+  resultBeforeOperator,
+  resultAfterOperator
+) {
+  const calcDescription = `${resultBeforeOperator} ${operator} ${resultAfterOperator}`;
+  outputResult(currentResult, calcDescription);
+}
 
-outputResult(currentResult,calculationDescription);
+function writeToLog(
+  operatorIdentifier,
+  prevNumber,
+  inputNumber,
+  currentNumber
+) {
+  const logEntryObject = {
+    operation: operatorIdentifier,
+    lastValue: prevNumber,
+    enteredNumber: inputNumber,
+    currentValue: currentNumber,
+  };
+  logEntries.push(logEntryObject);
+  console.log(logEntries);
+}
+
+function add() {
+  const operator = "+";
+  const enteredNum = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNum;
+  writeToLog("ADD", initialResult, enteredNum, currentResult);
+  createAndWriteOuptut(operator, initialResult, currentResult);
+}
+
+function subtract() {
+  const operator = "-";
+  const enteredNum = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNum;
+  writeToLog("SUBTRACT", initialResult, enteredNum, currentResult);
+  createAndWriteOuptut(operator, initialResult, currentResult);
+}
+
+function multiply() {
+  const operator = "*";
+  const enteredNum = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNum;
+  writeToLog("MULTIPLY", initialResult, enteredNum, currentResult);
+  createAndWriteOuptut(operator, initialResult, currentResult);
+}
+
+function divide() {
+  const operator = "/";
+  const enteredNum = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNum;
+  writeToLog("DIVIDE", initialResult, enteredNum, currentResult);
+  createAndWriteOuptut(operator, initialResult, currentResult);
+}
+
+function potencia() {
+  const operator = "^";
+  const enteredNum = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult ** enteredNum;
+  writeToLog("POTENCIA", initialResult, enteredNum, currentResult);
+  createAndWriteOuptut(operator, initialResult, currentResult);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+powerBtn.addEventListener("click", potencia);
